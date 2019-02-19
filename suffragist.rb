df1c0720280070708a395a909e6796892b8669ba
Emily Wilder <emilywilder.earth@gmail.com>
2019-02-19 11:23:53 -0500

    moved functionality into sinatra class

--- suffragist.rb
+++ suffragist.rb
@@ -8,26 +8,30 @@ Choices = {
     'NOO' => 'Noodles',
 }
 
-get '/' do
-  @title = 'Welcome to the Suffragist!'
-  erb :index
-end
+class Suffragist < Sinatra::Base
 
-post '/cast' do
-  @title = 'Thanks for casting your vote!'
-  @vote = params['vote']
-  @store = YAML::Store.new 'votes.yml'
-  @store.transaction do
-    @store['votes'] ||= {}
-    @store['votes'][@vote] ||= 0
-    @store['votes'][@vote] += 1
-  end
-  erb :cast
-end
+    get '/' do
+      @title = 'Welcome to the Suffragist!'
+      erb :index
+    end
 
-get '/results' do
-  @title = 'Results so far:'
-  @store = YAML::Store.new 'votes.yml'
-  @votes = @store.transaction { @store['votes'] }
-  erb :results
+    post '/cast' do
+      @title = 'Thanks for casting your vote!'
+      @vote = params['vote']
+      @store = YAML::Store.new 'votes.yml'
+      @store.transaction do
+        @store['votes'] ||= {}
+        @store['votes'][@vote] ||= 0
+        @store['votes'][@vote] += 1
+      end
+      erb :cast
+    end
+
+    get '/results' do
+      @title = 'Results so far:'
+      @store = YAML::Store.new 'votes.yml'
+      @votes = @store.transaction { @store['votes'] }
+      erb :results
+    end
+
 end
