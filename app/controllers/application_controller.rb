0020727a182e70bad0e8efea4f717a2a61404d14
Emily Wilder <emilywilder.earth@gmail.com>
2019-02-19 19:27:02 -0500

    fix indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,29 @@
 class ApplicationController < Sinatra::Base
 
-    configure do
-      set :views, 'app/views'
-      set :database_file, 'config/database.yml'
-    end
+  configure do
+    set :views, 'app/views'
+    set :database_file, 'config/database.yml'
+  end
 
-    get '/' do
-      @title = 'Welcome to the Suffragist!'
-      @choices = Choice.all
-      erb :index
-    end
+  get '/' do
+    @title = 'Welcome to the Suffragist!'
+    @choices = Choice.all
+    erb :index
+  end
 
-    post '/cast' do
-      @title = 'Thanks for casting your vote!'
-      choice = Choice.find(params["vote"].to_i)
-      @vote = choice.name
-      choice.votes += 1
-      choice.save
-      erb :cast
-    end
+  post '/cast' do
+    @title = 'Thanks for casting your vote!'
+    choice = Choice.find(params["vote"].to_i)
+    @vote = choice.name
+    choice.votes += 1
+    choice.save
+    erb :cast
+  end
 
-    get '/results' do
-      @title = 'Results so far:'
-      @choices = Choice.all
-      erb :results
-    end
+  get '/results' do
+    @title = 'Results so far:'
+    @choices = Choice.all
+    erb :results
+  end
 
 end
